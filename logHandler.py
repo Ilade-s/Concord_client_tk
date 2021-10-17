b587799c9854d4b7034ea43cb36f89fd1bf25a89
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-10-17 22:57:26 +0200

    les messages seront formatés pour passer dans la taille de la fenêtre

---
+++ logHandler.py
@@ -0,0 +1,11 @@
+"""
+Handler pour tout ce qui touche aux fichiers de log en json
+Permet d'interfacer sans lien direct avec la gui
+Sous la forme d'une classe, le construteur ouvre un fichier ou en ouvre un nouveau
+"""
+import json
+
+class LogHandler:
+
+    def __init__(self) -> None:
+        pass
